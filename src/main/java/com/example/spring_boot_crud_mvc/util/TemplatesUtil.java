569523d494fa235b0b14906a0158a08ab4741c1f
lastocheck <lastocheck@gmail.com>
2024-07-23 18:02:26 +0300

    Build header fragment

---
+++ src/main/java/com/example/spring_boot_crud_mvc/util/TemplatesUtil.java
@@ -0,0 +1,15 @@
+package com.example.spring_boot_crud_mvc.util;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Component
+public class TemplatesUtil {
+
+    public String getRolesStringFromAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        return authorities.stream().map(s -> s.getAuthority().replace("ROLE_", "")).collect(Collectors.joining(","));
+    }
+}
